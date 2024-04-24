83e8a1886e194ba5f077a5d73ab732d8838a635a
Payal Gupta <payalgupta@Payals-MacBook-Pro.local>
2024-04-24 11:21:31 +0530

    Fixed an error

--- src/pages/ImageGallery.tsx
+++ src/pages/ImageGallery.tsx
@@ -1,49 +1,20 @@
-import React, { useState } from 'react';
-import Lightbox from 'react-image-lightbox';
-import 'react-image-lightbox/style.css'; // This only needs to be imported once in your app
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 interface ImageGalleryProps {
     images: string[];
 }
 
 const ImageGallery: React.FC<ImageGalleryProps> = ({ images }) => {
-    const [photoIndex, setPhotoIndex] = useState(0);
-    const [isOpen, setIsOpen] = useState(false);
-
     return (
-        <>
-            <div className="grid grid-cols-3 gap-4">
-                {images.map((image, index) => (
-                    <div
-                        key={index}
-                        onClick={() => {
-                            setPhotoIndex(index);
-                            setIsOpen(true);
-                        }}
-                    >
-                        <img
-                            src={image}
-                            alt={`Image ${index + 1}`}
-                            style={{ width: '100%', cursor: 'pointer' }}
-                        />
-                    </div>
-                ))}
-            </div>
-            {isOpen && (
-                <Lightbox
-                    mainSrc={images[photoIndex]}
-                    nextSrc={images[(photoIndex + 1) % images.length]}
-                    prevSrc={images[(photoIndex + images.length - 1) % images.length]}
-                    onCloseRequest={() => setIsOpen(false)}
-                    onMovePrevRequest={() =>
-                        setPhotoIndex((photoIndex + images.length - 1) % images.length)
-                    }
-                    onMoveNextRequest={() =>
-                        setPhotoIndex((photoIndex + 1) % images.length)
-                    }
-                />
-            )}
-        </>
+        <Carousel>
+            {images.map((image, index) => (
+                <div key={index}>
+                    <img src={image} alt={`Image ${index + 1}`} />
+                </div>
+            ))}
+        </Carousel>
     );
 };
 
