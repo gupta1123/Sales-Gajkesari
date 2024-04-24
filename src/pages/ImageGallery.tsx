03a3506d152084789e07bffdddb3457d4f71710b
Payal Gupta <payalgupta@Payals-MacBook-Pro.local>
2024-04-24 11:33:50 +0530

    Fixed an error

--- src/pages/ImageGallery.tsx
+++ src/pages/ImageGallery.tsx
@@ -8,10 +8,28 @@ interface ImageGalleryProps {
 
 const ImageGallery: React.FC<ImageGalleryProps> = ({ images }) => {
     return (
-        <Carousel>
+        <Carousel
+            showThumbs={true}
+            thumbWidth={80}
+            renderThumbs={() =>
+                images.map((image, index) => (
+                    <div key={index}>
+                        <img
+                            src={image}
+                            alt={`Thumbnail ${index + 1}`}
+                            style={{ objectFit: 'cover', height: '60px' }}
+                        />
+                    </div>
+                ))
+            }
+        >
             {images.map((image, index) => (
                 <div key={index}>
-                    <img src={image} alt={`Image ${index + 1}`} />
+                    <img
+                        src={image}
+                        alt={`Image ${index + 1}`}
+                        style={{ width: '300px', height: '200px', objectFit: 'cover' }}
+                    />
                 </div>
             ))}
         </Carousel>
